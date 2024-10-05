e8c6e566ff6bc94f0b238bfed0373f7d18d595cc
Simon Vizner <viznertw@gmail.com>
2024-10-05 18:29:51 +0200

    automatically download LLM

--- src/launch.py
+++ src/launch.py
@@ -18,12 +18,18 @@ if not path.exists(environ["POST_LIST"]):
     Info("Creating new 'POST_LIST' file: '" + environ["POST_LIST"] + "'")
     f = open(environ["POST_LIST"], "w")
     f.close()
-    exit(0)
 
 if not path.exists(environ["LLM_PATH"]):
-    Error("Invalid 'LLM_PATH' path")
-    exit(0)
-
+    Error("Invalid 'LLM_PATH' path.\nCheck if you have downloaded a LLM model")
+    inp = input("Do you want to install LLM automatically? [Y/n]:")
+    if inp == "" or inp.lower().startswith("y"):
+        from urllib.request import urlretrieve
+        Info("Downloading 'TheBloke/Mistral-7B-Instruct-v0.2-GGUF' from 'https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/blob/main/mistral-7b-instruct-v0.2.Q4_K_M.gguf'")
+        urlretrieve("https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_K_M.gguf?download=true", environ["LLM_PATH"])
+        Info("Done downloading")
+    else:
+        exit(0)
+        
 if not path.exists(environ["YT_SECRET"]):
     Error("Invalid 'YT_SECRET' path")
     exit(0)
